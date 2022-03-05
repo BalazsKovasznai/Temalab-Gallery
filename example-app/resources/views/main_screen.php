53e3d1a685df561af0a0364dbff1dac56cf3b499
NemethKristof99 <nemethkristof980@gmail.com>
2022-03-05 13:33:11 +0100

    Home screen gombjai.

---
+++ example-app/resources/views/main_screen.php
@@ -0,0 +1,14 @@
+<?php ?>
+
+<html>
+    <body>
+
+        <form action="main_screen.php" method="get">
+                <input type="button" name="Login" value="Login">
+                <input type="button" name="Registration" value="Registration">
+        </form>
+
+    </body>
+
+
+</html>
