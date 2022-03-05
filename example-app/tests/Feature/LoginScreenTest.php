c2e6db10c31ab93fe14eed5920472a87ca100bd1
Balázs Kovásznai <balazskovasznai@edu.bme.hu>
2022-03-05 12:23:47 +0100

    1. teszt(home screen)

--- example-app/tests/Feature/LoginScreenTest.php
+++ example-app/tests/Feature/LoginScreenTest.php
@@ -3,18 +3,19 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class LoginScreenTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
-    public function test_the_application_returns_a_succeul_response()
+    public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->get('/home');
 
         $response->assertStatus(200);
     }
