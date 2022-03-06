eb2a7cefdf249bd6c462de3e9ffc59fd92e4705e
Balázs Kovásznai <balazskovasznai@edu.bme.hu>
2022-03-06 13:46:18 +0100

    login és registration button átírányítások

--- example-app/resources/views/home_screen.php
+++ example-app/resources/views/home_screen.php
@@ -4,8 +4,8 @@
     <body>
 
         <form action="main_screen.php" method="get">
-                <input type="button" name="Login" value="Login">
-                <input type="button" name="Registration" value="Registration">
+                <input type="button" name="Login" value="Login" onclick="location.href='/login_screen.php'">
+                <input type="button" name="Registration" value="Registration" onclick="location.href='/registration_screen.php'">
         </form>
 
     </body>
