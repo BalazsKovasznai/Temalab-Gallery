493a9ae475c858e120b89ea94bf1e50d4ddcdc3c
NemethKristof99 <nemethkristof980@gmail.com>
2022-03-06 15:26:05 +0100

    Login és registration képernyők

--- example-app/resources/views/login_screen.php
+++ example-app/resources/views/login_screen.php
@@ -3,6 +3,8 @@
     <body>
 
         <form action="login_screen.php" method="get">
+            <input type="text" name="Username" placeholder="Username">
+            <input type="password" name="Password" placeholder="Password">
             <input type="button" name="Login" value="Login" onclick="location.href='/folders'">
         </form>
 
--- example-app/resources/views/registration_screen.php
+++ example-app/resources/views/registration_screen.php
@@ -3,6 +3,8 @@
 <body>
 
 <form action="registration_screen.php" method="get">
+    <input type="text" name="Username" placeholder="Username">
+    <input type="password" name="Password" placeholder="Password">
     <input type="button" name="Registration" value="Registration" onclick="location.href='/login'">
 </form>
 
