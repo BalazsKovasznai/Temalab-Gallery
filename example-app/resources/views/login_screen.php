de7c33b83a7576f150ddd5743741eb3fe9fa2ce0
NemethKristof99 <nemethkristof980@gmail.com>
2022-03-10 16:00:49 +0100

    Login és registration képernyők szépítése

--- example-app/resources/views/login_screen.php
+++ example-app/resources/views/login_screen.php
@@ -1,12 +1,32 @@
 <?php ?>
 <html>
+    <head>
+        <style>
+            #textBoxes
+            {
+                position: absolute;
+                margin: 0;
+                top: 50%;
+                left: 50%;
+                -ms-transform: translate(-50%,-50%);
+                transform: translate(-50%,-50%);
+            }
+            button
+            {
+                font-size: 20px;
+                padding: 10px;
+                border-radius: 10px;
+                margin: 20px;
+            }
+        </style>
+    </head>
     <body>
-
-        <form action="login_screen.php" method="get">
+        <div id="textBoxes">
             <input type="text" name="Username" placeholder="Username">
             <input type="password" name="Password" placeholder="Password">
-            <input type="button" name="Login" value="Login" onclick="location.href='/folders'">
-        </form>
+            <button id="submitButton" onclick="location.href='/folders'"> Login </button>
+        </div>
+
 
     </body>
 
--- example-app/resources/views/registration_screen.php
+++ example-app/resources/views/registration_screen.php
@@ -1,12 +1,32 @@
 <?php ?>
 <html>
+<head>
+    <style>
+        #textBoxes
+        {
+            position: absolute;
+            margin: 0;
+            top: 50%;
+            left: 50%;
+            -ms-transform: translate(-50%,-50%);
+            transform: translate(-50%,-50%);
+        }
+        button
+        {
+            font-size: 20px;
+            padding: 10px;
+            border-radius: 10px;
+            margin: 20px;
+        }
+    </style>
+</head>
 <body>
 
-<form action="registration_screen.php" method="get">
-    <input type="text" name="Username" placeholder="Username">
-    <input type="password" name="Password" placeholder="Password">
-    <input type="button" name="Registration" value="Registration" onclick="location.href='/login'">
-</form>
+    <div id="textBoxes">
+        <input type="text" name="Username" placeholder="Username">
+        <input type="password" name="Password" placeholder="Password">
+        <button id="submitButton" onclick="location.href='/folders'"> Registration </button>
+    </div>
 
 </body>
 
