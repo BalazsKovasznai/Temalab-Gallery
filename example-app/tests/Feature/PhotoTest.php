717e96fab60d1ed1b01b29a8c5f19844c19bcd8b
vargaildiko <ildiko.varga.24@gmail.com>
2022-04-24 20:19:32 +0200

    PhotoTest javitás

--- example-app/tests/Feature/PhotoTest.php
+++ example-app/tests/Feature/PhotoTest.php
@@ -15,13 +15,38 @@ class PhotoTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_user_can_create_photo_in_album()
     {
-        $photo = \App\Models\Photo::factory()->create();
+        $user = User::factory()->create();
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
 
-        $response = $this->get('/albums/1');
+
+        $album = \App\Models\Album::factory()->create();
+
+        $response = $this->post('/albums/create', [
+            'name'=>$album->name,
+            'description'=>$album->description,
+            'cover_image'=>$album->cover_image,
+            'ulby'=>13,
+            'user_id'=>$user->id,
+        ]);
+
+        $photo = \App\Models\Photo::factory()->create();
+        $response = $this->post('/photos/create/$photo_id', [
+            'title'=>$photo->name,
+            'photo'=>$photo->image,
+            'size'=>5,
+            'description'=>$photo->name,
+            'album_id'=>$album->id,
+
+        ]);
+        $response = $this->get('/albums/$album_id');
         $response->assertSee($photo->title);
     }
+
 
     public function test_user_can_only_get_own_photos()
     {
